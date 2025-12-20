034b9984bd97345e2d3cfc6d13bb6cd9f6c02c18
Amruth S K <amruthsk25@gmail.com>
2025-12-20 20:54:54 +0530

    learn(dsa): Arrays

---
+++ 1_array/1_array.js
@@ -0,0 +1,16 @@
+//creating array
+let x = []; //empty array lenght 0
+var y = [1,23,4,false,'abc',null]; // length6
+let z = ['ab','cd','dc']; //length 3
+
+console.log(x,y,z); //same line
+
+
+console.log(x); //different line depends on terminal
+
+console.log(y); 
+
+console.log(z); 
+
+
+
---
+++ 1_array/2_mutability.js
@@ -0,0 +1,8 @@
+let str = 'Build';
+str[0] ='b';
+console.log(str);// immutable => no update
+
+let arr = [9,2,1,5,6]
+arr[3] = 34;
+
+console.log(arr);//mutable => updated
